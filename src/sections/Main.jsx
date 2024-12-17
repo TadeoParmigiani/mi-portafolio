1ce49c077194f9eccf01adf672621a1293c792c5
Tadeo Parmigiani <tadeoparmi2001@gmail.com>
2024-12-17 18:22:58 -0300

    4

--- src/sections/Main.jsx
+++ src/sections/Main.jsx
@@ -89,8 +89,8 @@ function UbicationWork() {
           Disponible para Trabajar
         </span>
         <a
-          href="/mi-portafolio/src/files/Tadeo-Parmigiani-CV-(1).pdf" 
-          download="Tadeo-Parmigiani-CV-(1).pdf"
+          href="https://github.com/TadeoParmigiani/mi-portafolio/blob/main/src/files/Tadeo-Parmigiani-CV-(1).pdf" 
+          download="Tadeo-Parmigiani-CV.pdf"
           className="h-8 cursor-pointer font-rubik rounded-md px-4 bg-blue-500 hover:bg-blue-800 dark:hover:bg-blue-700 transition-background duration-300 text-white flex items-center font-semibold"
         >
           Descargar CV
