496c15293d107e84807a3ac792880f026b60b8df
Tadeo Parmigiani <tadeoparmi2001@gmail.com>
2024-12-17 08:46:21 -0300

    2

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'https://TadeoParmigiani.github.io/mi-portafolio',
   resolve: {
     alias: {
       '@components': path.resolve(__dirname, 'src/components'),
